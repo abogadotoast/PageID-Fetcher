8238089b91f5f6818114649cffd0536105115dd9
battistone <rbattistone@unr.edu>
2015-05-29 17:24:22 -0700

    Update 4
    Fully functional and bug free! Ctrl-Shift-Z allows any user to find out
    the xID of a UNR page on Chrome. The addon isn't feature complete yet,
    but this is pretty nice.

--- pageidsearch.js
+++ pageidsearch.js
@@ -1,42 +1,74 @@
+// Key listeners for dealing with the keyboard.
+window.addEventListener("keydown", keysPressed, false);
+window.addEventListener("keyup", keysReleased, false);
+
+
 // Retrieves the xID found on UNR pages (the manifest controls page access).
 var bodyID = document.getElementsByTagName("body")[0].id;
-// Outputs the bodyID to console for testing.
-
-// stringify the bodyID
-
-console.log(bodyID);
+// This is an array which stores keypresses.
+var keys = [];
 
 
 
-// Copies the bodyID into the addon's HTML page, making it easy to copy and paste.
-document.body.onload = addElement(bodyID);
-//  Creates an alert box with a copy ID ready to be pasted.
-// copyToClipboard(bodyID);
-
-
-
-//  copy the bodyID into the addon page.
-
+// Trigger copy to clipboard upon hotkey.
 function copyToClipboard(bodyID)
 {
 	// Prompts a window with the XID.
-	window.prompt("Copy to clipboard: Ctrl+C, Enter", bodyID)
+	window.prompt("Copy to clipboard: Ctrl+C, Enter", bodyID);
 }
 
-function addElement (bodyID) 
-{ 
+// Controls actions upon keypresses. Takes global arguments.
+function keysPressed(e)
+{
+  // store an entry for every key pressed
+  keys[e.keyCode] = true;
 
+  
+  // Ctrl + Shift + Z
+  /*
+  if(keys[17] && keys[16] && keys[90])
+  {
+  // do something - interestingly enough, this works without needing to be called outside of the function.
+  copyToClipboard(bodyID);
+  // prevent default browser behavior
+  e.preventDefault();  
+  }
+  */
+  // Ctrl + Shift + Z
+  if(keys[17] && keys[16] && keys[90])
+  {
+    // do something - interestingly enough, this works without needing to be called outside of the function.
+    copyToClipboard(bodyID);
+    // prevent default browser behavior
+    e.preventDefault();
+    // this "releases" the key combination, so that there isnt an infinite loop of prompts
+    // this is used instead of the keysReleased function because we can specify multiple keys at once, versus last used.
+    keys[17] = false; // turns ctrl back to normal
+    keys[16] = false; // turns shift back to normal
+    keys[90] = false; // turns z back to normal
+  }
+}
+// If you're feeling lazy and just want to turn off the last used key.
+function keysReleased(e)
+{
+  // mark keys that were released
+  keys[e.keyCode] = false;
+}
+
+
+// I'll tinker with this tomorrow. Might still be useful to create an HTML page.
+/*
+function addElement (bodyIDArr) 
+{ 
   // create a new div element 
   // and give it some content 
   var newDiv = document.createElement("div"); 
 
-  var newContent = document.createTextNode(bodyID); 
+  var newContent = document.createTextNode(bodyIDArr); 
 
-  console.log(newContent); // works fine up to here?
   newDiv.appendChild(newContent); //add the text node to the newly created div. 
-  console.log(newDiv); // seems to be appended correctly. hmm.
   // add the newly created element and its content into the DOM 
   var currentDiv = document.getElementById("div1"); 
-  console.log(currentDiv); // this returns null. interesting.
   document.body.insertBefore(newDiv, currentDiv); 
 }
+*/
