0f2bafd3602090a74558b6a32e60544207e0ea6e
battistone <rbattistone@unr.edu>
2015-06-09 12:21:33 -0700

    Update 7
    Added rudimentary functionality for fetching PageIDs from
    unr.edu/nevada-today pages. Needs more work in getting it to display in
    the alert box upon keypress, but the logic is sound so far.

--- pageidsearch.js
+++ pageidsearch.js
@@ -2,11 +2,27 @@
 window.addEventListener("keydown", keysPressed, false);
 window.addEventListener("keyup", keysReleased, false);
 
-
 // Retrieves the xID found on UNR pages (the manifest controls page access).
 var bodyID = document.getElementsByTagName("body")[0].id;
 // This is an array which stores keypresses.
 var keys = [];
+
+// retrieves the header of pages so that they are available for parsing
+var currentLocation = window.location.href.toString().toLowerCase();
+console.log(currentLocation);
+// checks to see whether or not we're on most unr.edu pages except the one with nevada today.
+mainController(currentLocation);
+// checks to see if on a nevadatoday page and fills fullHeader with a massive string - this makes the addon more responsive on pages that don't require it.
+nevadaTodayController(currentLocation);
+
+// Controllers
+function mainController(currentLocation)
+{
+  if(currentLocation !="http://www.unr.edu/nevada-today")
+  {
+
+  }
+}
 
 // Trigger copy to clipboard upon hotkey.
 function copyToClipboard(bodyID)
@@ -42,20 +58,67 @@ function keysReleased(e)
   keys[e.keyCode] = false;
 }
 
-
-// I'll tinker with this tomorrow. Might still be useful to create an HTML page.
-/*
-function addElement (bodyIDArr) 
-{ 
-  // create a new div element 
-  // and give it some content 
-  var newDiv = document.createElement("div"); 
-
-  var newContent = document.createTextNode(bodyIDArr); 
-
-  newDiv.appendChild(newContent); //add the text node to the newly created div. 
-  // add the newly created element and its content into the DOM 
-  var currentDiv = document.getElementById("div1"); 
-  document.body.insertBefore(newDiv, currentDiv); 
+// Triggered upon loading nevadaToday, so that the slower pageID method is called
+function nevadaTodayController(currentLocation)
+{
+  console.log(currentLocation);
+  if(currentLocation==="http://www.unr.edu/nevada-today")
+  {
+  console.log("this works");
+  var fullHeader = document.getElementsByTagName("head")[0].innerHTML;
+  // once retrieved, this parses the fullHeader to fetch the PageID:
+  slowXIDFetch(fullHeader);
+  }
 }
-*/
+
+
+// Function that parses a header in HTML and returns an XID. 
+function slowXIDFetch(fullHeader)
+{
+  var fullHeaderLength = fullHeader.length;
+  // unicode literal for the spacebar space - see http://stackoverflow.com/questions/19810122/how-do-i-add-a-non-breaking-whitespace-in-javascript-without-using-innerhtml
+  var endOfXID = '.';
+  // go through element by element within the comment's head
+  
+  for(var i=0;i<fullHeaderLength;i++)
+  {
+    // This fetches the pageID from a comment in the header. It is really inefficient and it's really ugly - but it works.
+    if(fullHeader[i] == "P" && fullHeader[i+1] == "a" && fullHeader[i+2] == "g" && fullHeader[i+3] == "e" && fullHeader[i+4] == "I" && fullHeader[i+5] == "D" && fullHeader[i+6] == ":")
+      {
+        // i+7 is whitespace
+        // thus, we need to fetch the xID until the next whitespace
+        var start = i+7;
+        // i+8 is the first character, which is the x. hense why we're adding a count of 1.
+        var count = 1;
+        // this is the position of where the characters are added to the array.
+        var arraySize = 0;
+        // this is the array that stores the xid
+        var slowPageID = [];
+        // this is the string for slowPageID
+        var stringSlowPageID;
+
+        // we'll count up until we hit endOfXID
+        // while the elements in the array do not match the endOfXID character (which is a period)
+        while(fullHeader[start+count] !=  endOfXID)
+        {
+          // log what is stored at that element
+          slowPageID[arraySize] = fullHeader[start+count];
+          console.log(fullHeader[start+count]);
+          // keep counting up until we hit endOfXID
+          count++;
+          // move the arraysize up by one to store the next character there.
+          arraySize++;
+        }
+
+        // turns the array of character into a string  and also removes the commas
+        stringSlowPageID = slowPageID.join('');
+        console.log(stringSlowPageID);
+
+      } 
+
+  }
+  
+  // check to see if "pageID" is there
+
+  // once "pageID" is found, send a console.log out
+}
